2eb1282caf977dcc58ec99b64ca52dbffc4cff96
aiNko <44474824+salessebo@users.noreply.github.com>
2022-05-02 00:41:00 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def hello():
+    if request.method == "POST":
+        # file_html_data = open('./data_html.txt', "w")
+        # file_simple_data = open('./simple_data.txt', "w")
+        # data_html = request.form.get('data_html')
+        # data_simple = request.form.get('data_simple')
+        # file_html_data.write(data_html)
+        # file_simple_data.write(data_simple)
+        # file_html_data.close()
+        # file_simple_data.close()
+        print("Updated!")
+        return("Updated!")
+    
+    if request.method == 'GET':
+        return 'Hello Bitches'
+
+
+if __name__ == "__main__":
+    app.run()
+
