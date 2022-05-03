ffdfa4dad767b404553cd6a619a3b2ab79e9963c
aiNko <44474824+salessebo@users.noreply.github.com>
2022-05-03 00:17:17 -0400

    Update parsers.py

--- parsers.py
+++ parsers.py
@@ -1,8 +1,7 @@
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 import pandas as pd
 
 def parse_throne(html_data):
     table_data = pd.read_html(html_data)
     print(table_data)
-    return table_data
 
