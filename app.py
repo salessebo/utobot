46ada91c9e29c869b0a365ee8377c5ac7c30f83b
aiNko <44474824+salessebo@users.noreply.github.com>
2022-05-02 21:27:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello():
         url = request.form.get('url')
         prv = request.form.get('prov')
         res = f'Received {prv} data from {url}...'
-        with open('./data_html.txt', "w") as data_file:
+        with open('data_html.txt', "w") as data_file:
             data_file.write(res)
         return res
 #         file_html_data = open('./data_html.txt', "w")
