91c06573ab929b45abe92d6135b2d974e6f93e51
aiNko <44474824+salessebo@users.noreply.github.com>
2022-05-03 00:16:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,24 +12,15 @@ def hello():
         prv = request.form.get('prov')
         res = f'Received {prv} data from {url}...'
         print(res)
-
-        html_data = request.form.get('html_data')
-        data = parse_throne(html_data)
-
-
-
-        with open('data_html.txt', "w") as data_file:
-            data_file.write(data)
-        with open('data_html.txt', "r") as data_file:
-            res2 = data_file.read()
+        if 'throne' in url:
+            html_data = request.form.get('html_data')
+            parse_throne(html_data)
+            
         return res2
 
     
     if request.method == 'GET':
-        with open('data_html.txt', "r") as data_file:
-            res2 = data_file.read()
-            return res2
-        # return 'Receiving end'
+        return 'Receiving end'
 
 
 if __name__ == "__main__":
