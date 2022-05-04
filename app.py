4ce593024bd7459bc1c0694de7f0e694fc79ea6e
aiNko <44474824+salessebo@users.noreply.github.com>
2022-05-04 02:48:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from cgitb import html
 from flask import Flask, request
 from datetime import datetime
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 
 
 
@@ -10,15 +10,16 @@ def parse_throne(html_data):
     # Parse throne table
 #     with open("throne_html.txt", "r") as throne_html:
 #         html_data = throne_html.read()
-    soup = BeautifulSoup(html_data, 'html.parser')
     table_data = pd.read_html(html_data)
     df1 = table_data[1][[0,1]]
     df2 = table_data[1][[2,3]]
     df2.columns= [0,1]
     throne_data = pd.concat([df1,df2], ignore_index=True).set_index(0)[1]
-    throne_data['Message'] = [x.get_text() for x in soup.find_all("p", {"class": "advice-message"})]
-    spells = [x.get_text(strip=True) for x in soup.find_all("p") if x.get_text(strip=True).startswith('Spell')][0].split(':')[1]
-    throne_data['Spells'] = [x.split('(') for x in spells.split(')')][:-1]
+    
+#     soup = BeautifulSoup(html_data, 'html.parser')
+#     throne_data['Message'] = [x.get_text() for x in soup.find_all("p", {"class": "advice-message"})]
+#     spells = [x.get_text(strip=True) for x in soup.find_all("p") if x.get_text(strip=True).startswith('Spell')][0].split(':')[1]
+#     throne_data['Spells'] = [x.split('(') for x in spells.split(')')][:-1]
     return throne_data.to_dict()
 
     
